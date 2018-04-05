4948817070164c4efd4d9042735b789c64f0837c
VivereSV <32445070+VivereSV@users.noreply.github.com>
2018-04-05 08:13:01 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -398,6 +398,9 @@ client.on("message", async message => {
 
 
                     var val = 0;
+                    console.log("R: " + r);
+                    console.log("C: " + c);
+                    console.log("Rows: " + rows[r][c]);
                     if(rows[r][c].length > 0) {
                         console.log("Value: " + rows[r][c]);
                         val = rows[r][c];
