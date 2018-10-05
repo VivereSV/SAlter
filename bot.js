24ef679b555b5d778b2d82c69253eae3d007863d
Sean Chang <seanchang9@gmail.com>
2018-10-04 22:13:24 -0400

    ree

--- bot.js
+++ bot.js
@@ -565,6 +565,7 @@ client.on("message", async message => {
     if (message.channel.name === "general" || message.channel.name === "granblue_discussion" || message.channel.name === "public_scrim") return;
 
     if (command === "ready") {
+        console.log(args[0])
         Spreadsheet.load({
             debug: true,
             spreadsheetName: 'SVWL',
@@ -578,7 +579,6 @@ client.on("message", async message => {
             function sheetReady(err, spreadsheet) {
                 if (err) throw err;
                 spreadsheet.receive({ getValues: true }, function (err, rows, info) {
-                    console.log(args[0]);
                     if (err) throw err;
                     if(rows[0][0] == undefined) {
                         spreadsheet.add({ 1: { 1: message.author.id}});
